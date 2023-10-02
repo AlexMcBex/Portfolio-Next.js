e7e8c6b81762d9733c489d3a670ec90dc31a2e66
AlexMcBex <alessiobelli94@gmail.com>
2023-10-02 10:54:43 -0400

    added burger button style

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,26 +13,42 @@ export default function Navbar() {
                 <>
                     <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                         <div className="flex justify-between h-16">
-                        <div className="flex justify-between w-full">
-                            <div className="flex items-center">
-                                <Link href="/">
-                                    <h1 className="text-2xl font-medium">
-                                        Alex <span className="text-teal-500">Belli</span>
-                                    </h1>
-                                </Link>
+                            <div className="flex justify-between w-full">
+                                <div className="flex items-center">
+                                    {/* main left */}
+                                    <Link href="/">
+                                        <h1 className="text-2xl font-medium">
+                                            Alex <span className="text-teal-500">Belli</span>
+                                        </h1>
+                                    </Link>
+                                </div>
+                                {/* links right */}
+                                <div className="hidden sm:ml-6 sm:flex sm:space-x-8 sm:items-center">
+                                    <Link href="/" prefetch className={`${pathname === "/" ? 'border-teal-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium' : 'border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-thite inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'}`}>
+                                        Home
+                                    </Link>
+                                    <Link href="/guestbook" prefetch className={`${pathname === "/guestbook" ? 'border-teal-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium' : 'border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-thite inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'}`}>
+                                        Guestbook
+                                    </Link>
+                                    <Link href="/projects" prefetch className={`${pathname === "/projects" ? 'border-teal-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium' : 'border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-thite inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'}`}>
+                                        Projects
+                                    </Link>
+                                </div>
                             </div>
-                            <div className="hidden sm:ml-6 sm:flex sm:space-x-8 sm:items-center">
-                                <Link href="/" prefetch  className={`${pathname === "/" ? 'border-teal-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium' : 'border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-thite inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'}`}>
-                                Home
-                                </Link>
-                                <Link href="/guestbook" prefetch  className={`${pathname === "/guestbook" ? 'border-teal-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium' : 'border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-thite inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'}`}>
-                                Guestbook
-                                </Link>
-                                <Link href="/projects" prefetch  className={`${pathname === "/projects" ? 'border-teal-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium' : 'border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-thite inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'}`}>
-                                Projects
-                                </Link>
+                            {/* burger option */}
+                            <div className="-mr-2 flex items-center sm:hidden">
+                                <Disclosure.Button className="inline-flex items-center p-2 justify-center rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-teal-500 dark:hover:bg-gray-800">
+                                    {open ? (
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    ) : (
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                        </svg>
+                                    )}
+                                </Disclosure.Button>
                             </div>
-                        </div>
                         </div>
                     </div>
                 </>
